2ed3097962edb6d6d1b0e3b94851c30dc2855494
Amr Walidi <amr.nawaf@gmail.com>
2023-09-05 16:01:33 +0300

    h

--- src/Components/Home/ProfileImage.tsx
+++ src/Components/Home/ProfileImage.tsx
@@ -64,7 +64,7 @@ function ProfileImage() {
         />
       </svg>
       <div>
-        <img src="./src/assets/ProfileImage.png" alt="My Portrait" />
+        <img src="/Images/ProfileImage.png" alt="My Portrait" />
       </div>
     </div>
   );
