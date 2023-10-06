b2c2c5cd96c5f885d91cc65c244d609057ec69fd
Amr Walidi <amr.nawaf@gmail.com>
2023-10-06 20:13:07 +0300

    website completed

--- src/Components/Contact/Form.tsx
+++ src/Components/Contact/Form.tsx
@@ -1,21 +1,103 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 function Form() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const [errors, setErrors] = useState({
+    count: 0,
+    name: false,
+    email: false,
+    message: false,
+  });
+
+  const [valideForm, setValideForm] = useState(false);
+
+  useEffect(() => {
+    if (errors.count !== 0) {
+      setValideForm(!(errors.name || errors.email || errors.message));
+    }
+  }, [errors]);
+
+  const handleSbmit = () => {
+    setErrors({
+      count: 1,
+      name: formData.name.length === 0,
+      email: formData.email.length === 0 || !formData.email.includes("@"),
+      message: formData.message.length === 0,
+    });
+  };
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const input = document.getElementById(event.target.id);
+    input?.classList.remove("error");
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  if (valideForm)
+    return <div className="send-message">Thank you for your message :)</div>;
+
   return (
     <div>
       <form>
         <div className="top-inputs">
-          <input type="text" name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
+          <div>
+            <input
+              className={errors.name ? "error" : ""}
+              type="text"
+              value={formData.name}
+              id="name"
+              name="name"
+              placeholder="Name"
+              onChange={handleChange}
+            />
+            {errors.name && <p className="error">This field is required</p>}
+          </div>
+          <div>
+            <input
+              className={errors.email ? "error" : ""}
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            {errors.email && (
+              <p className="error">An email must include @ symbol</p>
+            )}
+          </div>
         </div>
-        <input type="text" name="subject" placeholder="Subject" />
         <input
           type="text"
-          name="message"
-          placeholder="Write Your Message Here"
-        ></input>
-        <div className="submit-button-container">
+          id="subject"
+          name="subject"
+          placeholder="Subject"
+          value={formData.subject}
+          onChange={handleChange}
+        />
+        <div>
+          <input
+            className={errors.message ? "error" : ""}
+            type="text"
+            id="message"
+            name="message"
+            placeholder="Write Your Message Here"
+            value={formData.message}
+            onChange={handleChange}
+          ></input>
+          {errors.message && <p className="error">This field is required</p>}
+        </div>
+        <div onClick={handleSbmit} className="submit-button-container">
           <div className="submit-button">
             <div className="contact-icon-container ">
               <FontAwesomeIcon className="contact-icon" icon={faPaperPlane} />
